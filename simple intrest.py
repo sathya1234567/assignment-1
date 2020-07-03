e4904b73c571dde7741e8e6f24233fadefbda0a0
sathya1234567 <67158385+sathya1234567@users.noreply.github.com>
2020-07-03 11:56:24 +0530

    Add files via upload

---
+++ simple intrest.py
@@ -0,0 +1,7 @@
+p = float(input("Enter the principal amount: "))
+t = float(input("Enter the time in years: "))
+r = float(input("Enter the interest rate: "))
+
+simple_interest = p * t * r / 100
+
+print("Simple Interest = %.2f" %simple_interest)
